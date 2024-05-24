f45d9380614f0908810d210361cfece7b1902596
Sedami Dietrich Montcho <didimontcho@gmail.com>
2024-05-24 17:10:12 -0300

    movimentacao e login

--- authors/views.py
+++ authors/views.py
@@ -1,23 +1,40 @@
+from django.contrib import messages
+from django.http import Http404
 from django.shortcuts import redirect, render
+from django.urls import reverse
 
 from .forms import RegisterForm
 
 
 def register_view(request):
-
     register_form_data = request.session.get('register_form_data', None)
-    form = RegisterForm()
+    form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
+        'form_action': reverse('authors:create'),
+        'form_action': reverse('authors:register_create'),
     })
 
 
 def register_create(request):
-
     if not request.POST:
         raise Http404()
-
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
+    if form.is_valid():
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
+        messages.success(request, 'Your user is created, please log in.')
+        del (request.session['register_form_data'])
+
     return redirect('authors:register')
+
+
+def login_view(request):
+    return render(request, 'authors/pages/login.html')
+
+
+def login_create(request):
+    return render(request, 'authors/pages/login.html')
